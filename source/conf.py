572eb3ede6045b4bec6d02f43561dd53185aee8c
wikwyd <284385@student.pwr.edu.pl>
2026-06-28 22:00:30 +0000

    update

--- source/conf.py
+++ source/conf.py
@@ -8,7 +8,7 @@
 
 project = 'Sprawozdanie-284385'
 copyright = '2026, Olaf Chomicki, Konrad Machowski, Wiktor Wydrzyński'
-author = 'Olaf Chomicki, Konrad Machowski, Wiktor Wydrzyński'
+author = 'Wiktor Wydrzyński'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
